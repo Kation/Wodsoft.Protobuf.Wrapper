9129aebc283a0d5ca3b9ad128567fcdf4b9454b8
Kation <kation@outlook.com>
2020-11-22 13:38:06 +0800

    Move something Message<> to Message

--- src/Wodsoft.Protobuf.Extensions/Message.cs
+++ src/Wodsoft.Protobuf.Extensions/Message.cs
@@ -7,7 +7,74 @@ using System.Text;
 
 namespace Wodsoft.Protobuf
 {
-    public abstract class Message<T> : IMessage, IBufferMessage
+    public abstract class Message : IMessage, IBufferMessage
+    {
+        MessageDescriptor IMessage.Descriptor => null;
+
+        protected abstract int CalculateSize();
+
+        protected abstract void Write(ref WriteContext writer);
+
+        protected abstract void Read(ref ParseContext parser);
+
+        #region IMessage Implementation
+
+        int IMessage.CalculateSize()
+        {
+            return CalculateSize();
+        }
+
+        void IMessage.MergeFrom(CodedInputStream input)
+        {
+            input.ReadRawMessage(this);
+        }
+
+        void IMessage.WriteTo(CodedOutputStream output)
+        {
+            output.WriteRawMessage(this);
+        }
+
+        #endregion
+
+        #region IBufferMessage Implementation
+
+        void IBufferMessage.InternalMergeFrom(ref ParseContext ctx)
+        {
+            Read(ref ctx);
+        }
+
+        void IBufferMessage.InternalWriteTo(ref WriteContext ctx)
+        {
+            Write(ref ctx);
+        }
+
+        #endregion
+
+        public static void Serialize<T>(Stream stream, T source)
+            where T : class, new()
+        {
+            Message<T>.Serialize(stream, source);
+        }
+
+        public static void Serialize<T>(CodedOutputStream output, T source)
+            where T : class, new()
+        {
+            Message<T>.Serialize(output, source);
+        }
+        public static T Deserialize<T>(Stream stream)
+            where T : class, new()
+        {
+            return Message<T>.Deserialize(stream);
+        }
+
+        public static T Deserialize<T>(CodedInputStream input)
+            where T : class, new()
+        {
+            return Message<T>.Deserialize(input);
+        }
+    }
+
+    public abstract class Message<T> : Message
         where T : class, new()
     {
         internal static Type MessageType;
@@ -43,11 +110,19 @@ namespace Wodsoft.Protobuf
 
         public static void Serialize(Stream stream, T source)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
             Serialize(new CodedOutputStream(stream, true), source);
         }
 
         public static void Serialize(CodedOutputStream output, T source)
         {
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
             Message<T> message = source;
             output.WriteRawMessage(message);
             output.Flush();
@@ -55,23 +130,20 @@ namespace Wodsoft.Protobuf
 
         public static T Deserialize(Stream stream)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
             return Deserialize(new CodedInputStream(stream, true));
         }
 
         public static T Deserialize(CodedInputStream input)
         {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
             Message<T> message = (Message<T>)Activator.CreateInstance(MessageBuilder.GetMessageType<T>());
             input.ReadRawMessage(message);
             return message.Source;
         }
 
-        MessageDescriptor IMessage.Descriptor => null;
-
-        protected abstract int CalculateSize();
-
-        protected abstract void Write(ref WriteContext writer);
-
-        protected abstract void Read(ref ParseContext parser);
 
         public static implicit operator Message<T>(T source)
         {
@@ -88,38 +160,5 @@ namespace Wodsoft.Protobuf
                 throw new ArgumentNullException(nameof(message));
             return message.SourceValue;
         }
-
-        #region IMessage Implementation
-
-        int IMessage.CalculateSize()
-        {
-            return CalculateSize();
-        }
-
-        void IMessage.MergeFrom(CodedInputStream input)
-        {
-            input.ReadRawMessage(this);
-        }
-
-        void IMessage.WriteTo(CodedOutputStream output)
-        {
-            output.WriteRawMessage(this);
-        }
-
-        #endregion
-
-        #region IBufferMessage Implementation
-
-        void IBufferMessage.InternalMergeFrom(ref ParseContext ctx)
-        {
-            Read(ref ctx);
-        }
-
-        void IBufferMessage.InternalWriteTo(ref WriteContext ctx)
-        {
-            Write(ref ctx);
-        }
-
-        #endregion
     }
 }
