3e2a62c93f133681821391f697c50b93b01a6f26
Kation <kation@outlook.com>
2021-09-24 19:15:04 +0800

    Fix null byte array value calculate size

--- src/Wodsoft.Protobuf.Wrapper/Generators/ByteArrayCodeGenerator.cs
+++ src/Wodsoft.Protobuf.Wrapper/Generators/ByteArrayCodeGenerator.cs
@@ -17,6 +17,22 @@ namespace Wodsoft.Protobuf.Generators
         public override WireFormat.WireType WireType => WireFormat.WireType.LengthDelimited;
 
         /// <inheritdoc/>
+        public override void GenerateCalculateSizeCode(ILGenerator ilGenerator, LocalBuilder valueVariable, int fieldNumber)
+        {
+            var end = ilGenerator.DefineLabel();
+            var next = ilGenerator.DefineLabel();
+            ilGenerator.Emit(OpCodes.Ldloc, valueVariable);
+            ilGenerator.Emit(OpCodes.Brtrue, next);
+            ilGenerator.Emit(OpCodes.Ldc_I4_0);
+            ilGenerator.Emit(OpCodes.Br, end);
+            ilGenerator.MarkLabel(next);
+            GenerateCalculateSizeCode(ilGenerator, valueVariable);
+            ilGenerator.Emit(OpCodes.Ldc_I4, CodedOutputStream.ComputeTagSize(fieldNumber));
+            ilGenerator.Emit(OpCodes.Add_Ovf);
+            ilGenerator.MarkLabel(end);
+        }
+
+        /// <inheritdoc/>
         protected override void GenerateCalculateSizeCode(ILGenerator ilGenerator, LocalBuilder valueVariable)
         {
             ilGenerator.Emit(OpCodes.Ldloc, valueVariable);
